7b6b1adb7ed981f0e68aedf3166a1079601e9604
Thiago Henrique Hüpner <thihup@gmail.com>
2022-06-28 16:52:39 +0000

    Use JDK 17, Nashorn-core and JUnit 5

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,22 +40,20 @@
  */
 package com.mycompany.app;
 
-import static org.junit.Assert.fail;
-import org.junit.Assume;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-public class AppTest {
+class AppTest {
     private static long nashorn;
 
-    @BeforeClass
-    public static void nashornBenchmark() throws Exception {
+    @BeforeAll
+    static void nashornBenchmark() throws Exception {
         nashorn = App.benchNashornScriptEngine();
     }
 
     @Test
-    public void testGraalPolyglotSpeed() throws Exception {
-        assertGraalVMOrJDK11();
+    void testGraalPolyglotSpeed() throws Exception {
         long graalJS = App.benchGraalPolyglotContext();
         if (nashorn < graalJS) {
             fail(String.format("Graal.js (%d ms) should be faster than Nashorn (%d ms).", graalJS, nashorn));
@@ -63,22 +61,10 @@ public class AppTest {
     }
 
     @Test
-    public void testGraalScriptEngineSpeed() throws Exception {
-        assertGraalVMOrJDK11();
+    void testGraalScriptEngineSpeed() throws Exception {
         long graalJS = App.benchGraalScriptEngine();
         if (nashorn < graalJS) {
             fail(String.format("Graal.js (%d ms) should be faster than Nashorn (%d ms).", graalJS, nashorn));
-        }
-    }
-
-    private void assertGraalVMOrJDK11() {
-        if (System.getProperty("java.vm.name").contains("GraalVM")) {
-            return;
-        }
-        try {
-            Class.forName("java.lang.Module");
-        } catch (ClassNotFoundException ex) {
-            Assume.assumeNoException("Skipping the test on regular JDK8", ex);
         }
     }
 }
